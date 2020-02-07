35e7aa529a84a15b5f174cbeedf9da455d8e873d
Adam HAMBE <adam.hambe@alten.se>
2020-02-07 16:05:21 +0100

    Part: 90

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       {id: 'asfds', name: 'Adam', age: 30},
@@ -13,6 +18,19 @@ class App extends Component {
     ],
     showPersons: false
 
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log('[App.js] componentWillMount')
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
   }
 
   nameChangedHandler = (event, id) => {
@@ -46,8 +64,7 @@ class App extends Component {
   }
 
   render() {
-    
-
+    console.log('[App.js] render')
     let persons = null;
 
     if ( this.state.showPersons ) {

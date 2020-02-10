595ae2ebffb3e1abb030fb39b30802afc645f681
Adam HAMBE <adam.hambe@alten.se>
2020-02-10 16:06:28 +0100

    102-105

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react'
 import classes from './Person.css'
+import Aux from '../../../hoc/Auxiliary'
 
 class Person extends Component {
 
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <div className={classes.Person}>
+            // Can also use <Fragment></Fragment> for the same effect as <Aux></Aux>
+            <Aux>
                 <p onClick={this.props.click}>Hej jag heter {this.props.name} och är {this.props.age} år gammal</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>   
+            </Aux>
         )
     }
 }
---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+const aux = props => props.children;
+
+export default aux
+
+// hoc (higher order component) wrap another component

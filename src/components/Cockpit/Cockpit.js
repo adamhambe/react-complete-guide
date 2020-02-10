4a1d905a8207efaedfaa892ac7eb59eccd76750c
Adam HAMBE <adam.hambe@alten.se>
2020-02-10 14:39:02 +0100

    98-99

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -38,10 +38,10 @@ const cockpit = (props) => {
         btnClass = classes.Red;
     }
 
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
       assignedClasses.push(classes.red) // classes = ['red']
     }
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
       assignedClasses.push(classes.bold) //classes = ['red', 'bold']
     }
 
@@ -57,4 +57,7 @@ const cockpit = (props) => {
     )
 }
 
-export default cockpit
+// Wrap components in React.memo() optimizes performance by taking a snapshot
+// If said snapshot shows a change then it will re-render but if it 
+// doesn't it will skip this unnecessary step
+export default React.memo(cockpit)
